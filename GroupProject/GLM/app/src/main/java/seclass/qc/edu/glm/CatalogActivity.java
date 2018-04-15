5c77ec31188d75e7db8f54a047f36fbb6117cab6
Jherico-Ryan Tapang <jhericot@outlook.com>
2018-04-14 21:15:33 -0400

    working on java files

--- GroupProject/GLM/app/src/main/java/seclass/qc/edu/glm/CatalogActivity.java
+++ GroupProject/GLM/app/src/main/java/seclass/qc/edu/glm/CatalogActivity.java
@@ -1,4 +1,52 @@
 package seclass.qc.edu.glm;
 
-public class CatalogActivity {
+import android.app.LoaderManager;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class CatalogActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //show view
+        setContentView(R.layout.activity_show_catalog);
+
+        //set up the FAB for CreateListActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //switch to this view
+                Intent intent = new Intent(CatalogActivity.this, CreateListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 }
